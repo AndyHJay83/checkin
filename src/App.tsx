3879b26d51a796b512f20dee9c7933f1200f59dc
AndyHJay83 <120287095+AndyHJay83@users.noreply.github.com>
2025-04-19 10:02:01 +0100

    Fix

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route } from 'react-router-dom';
 import CheckInPage from './pages/CheckInPage';
 import SettingsPage from './pages/SettingsPage';
 import Navbar from './components/Navbar';
